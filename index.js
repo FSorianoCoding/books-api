21ed9807a242f1e303d798278dab99f10bf0e19a
Fabian Soriano <fabo.soriano@gmail.com>
2022-10-11 00:06:07 -0700

    added controller routes

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     console.log('connected to mongo on: ', process.env.MONGO_URI)
 })
 
-// ROOT INDEX
+// ROOT INDEX 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Hello World')
 })
 
 // BOOKS 
